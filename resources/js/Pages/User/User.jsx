988f317d6efb276bf4c0c1ce8158ee29b18bea3f
priana13 <herian.sap@gmail.com>
2024-10-10 20:50:40 +0700

    hapus data

--- resources/js/Pages/User/User.jsx
+++ resources/js/Pages/User/User.jsx
@@ -3,9 +3,11 @@ import { useEffect , useState } from 'react';
 
 import { Input } from 'tw-elements';
 
+import { useForm } from "@inertiajs/react";
+
+
 export default function User({users}){
 
-    // console.log(users)
 
     return (
 
@@ -16,7 +18,7 @@ export default function User({users}){
 
                 <Link
                 href="/users/create"
-                class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">
+                className="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">
                 Create
                 </Link>
 
@@ -39,28 +41,34 @@ export default function User({users}){
                             </tr>
                         </thead>
                         <tbody>
-                            {users.data.map( user => (<>                            
+                            {users.data.map( user => (                           
                             
-                                <tr className="border-b border-neutral-200 dark:border-white/10">
+                                <tr className="border-b border-neutral-200 dark:border-white/10" key={user.id}>
                                     <td className="whitespace-nowrap px-6 py-4 font-medium">{user.id}</td>
                                     <td className="whitespace-nowrap px-6 py-4">{user.name}</td>
                                     <td className="whitespace-nowrap px-6 py-4">{user.email}</td>
-                                    <td className="whitespace-nowrap px-6 py-4">
+                                    <td className="whitespace-nowrap py-4 flex">
                                     <Link
+                                        key={user.id}
                                         href={'users/' + user.id}
-                                        class="inline-block rounded bg-success px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-success-3 transition duration-150 ease-in-out hover:bg-success-accent-300 hover:shadow-success-2 focus:bg-success-accent-300 focus:shadow-success-2 focus:outline-none focus:ring-0 active:bg-success-600 active:shadow-success-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong mx-2">
+                                        className="inline-block rounded bg-success px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-success-3 transition duration-150 ease-in-out hover:bg-success-accent-300 hover:shadow-success-2 focus:bg-success-accent-300 focus:shadow-success-2 focus:outline-none focus:ring-0 active:bg-success-600 active:shadow-success-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">
                                         Show
-                                    </Link>
-                                    <Link
-                                        href="/users/create"
-                                        class="inline-block rounded bg-danger px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-danger-3 transition duration-150 ease-in-out hover:bg-danger-accent-300 hover:shadow-danger-2 focus:bg-danger-accent-300 focus:shadow-danger-2 focus:outline-none focus:ring-0 active:bg-danger-600 active:shadow-danger-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">
+                                    </Link>                                  
+
+                                    <Link                                          
+                                        href={`/users/${user.id}`}
+                                        key={`key-${user.id}`}  
+                                        method="DELETE"      
+                                        as="button"                                   
+                                        className="inline-block rounded bg-danger px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-danger-3 transition duration-150 ease-in-out hover:bg-danger-accent-300 hover:shadow-danger-2 focus:bg-danger-accent-300 focus:shadow-danger-2 focus:outline-none focus:ring-0 active:bg-danger-600 active:shadow-danger-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong mx-2">
                                         Delete
-                                    </Link>
+                                    </Link> 
+
 
                                     </td>
                                 </tr>
 
-                            </>) )}                        
+                          ) )}                        
 
                          
                         </tbody>
@@ -82,6 +90,7 @@ export default function User({users}){
 
                                 <li>
                                     <Link
+                                        key={link.label}
                                         className="relative block rounded bg-transparent px-5 py-3 text-lg text-surface transition duration-300 hover:bg-neutral-100 focus:bg-neutral-100 focus:text-primary-700 focus:outline-none active:bg-neutral-100 active:text-primary-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700  dark:focus:text-primary-500 dark:active:bg-neutral-700 dark:active:text-primary-500"
                                         href={link.url}
                                         >{link.label}
